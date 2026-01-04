958c419b46c931952394e365f4681789db42dab1
henryv2003 <alexandervh1986@gmail.com>
2026-01-04 15:58:29 +0100

    path error fixed

--- src/components/Astronaut.jsx
+++ src/components/Astronaut.jsx
@@ -13,7 +13,7 @@ import { useFrame } from '@react-three/fiber';
 
 export function Astronaut(props) {
   const group = useRef()
-  const { nodes, materials, animations } = useGLTF('/models/tenhun_falling_spaceman_fanart.glb')
+  const { nodes, materials, animations } = useGLTF('./models/tenhun_falling_spaceman_fanart.glb')
   const { actions } = useAnimations(animations, group);
   useEffect(()=> {
     if(animations.length > 0){
